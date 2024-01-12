e97a74a67e4f57fef747ab4ac917d7e8933f1b7a
saifrajput98 <saifrajput98@gmail.com>
2024-01-12 19:29:36 +0500

    Basic Operators Assignment

---
+++ site/index.js
@@ -0,0 +1,20 @@
+// let stdName = 'Saif';
+// let stdAge = 25;
+// console.log('My Name is ' + stdName + ' ' + 'And my age is ' + stdAge);
+
+let population = 120000000;
+console.log(population / 2);
+population++;
+console.log(population);
+let popFinlannd = 6000000;
+console.log(population > popFinlannd);
+let avgPopulation = 33000000;
+console.log(population < avgPopulation);
+
+let myCountry = 'Pakistan';
+let continent = 'Asia';
+let myCountPopulation = '1100000';
+let language = 'Urdu';
+
+let description = myCountry + ' is in ' + continent + ' and its ' + myCountPopulation + ' people speak ' + language;
+console.log(description);

efb9410f144619c7160b06a556f2b04e7043c83f
saifrajput98 <saifrajput98@gmail.com>
2024-02-13 00:07:39 +0500

    Added functionaloty to show and remove modal

--- modal/script.js
+++ modal/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let btnCloseModal = document.querySelector('.close-modal');
+let btnShowModal = document.querySelectorAll('.show-modal');
+
+let showModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+let closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnShowModal.length; i++) {
+    btnShowModal[i].addEventListener('click', showModal);
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+}
